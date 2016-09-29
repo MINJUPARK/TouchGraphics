ba620fe77c6d600b23284632e7ec25003ba625eb
minju0227@e-mirim.hs.kr <minju0227@e-mirim.hs.kr>
2016-09-29 20:02:59 +0900

    #5 drawing paper add

--- app/src/main/java/com/example/touchgraphics/MainActivity.java
+++ app/src/main/java/com/example/touchgraphics/MainActivity.java
@@ -7,30 +7,52 @@ import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int LINE=1, RECT=2, CRICLE=3;
+    DrawShape ds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new DrawShape(getApplicationContext()));
+        setContentView(R.layout.activity_main);
+        ds = new DrawShape(getApplicationContext());
+        LinearLayout linear = (LinearLayout) findViewById(R.id.linear_view);
+        linear.addView(ds);
     }
-    class DrawShape extends View {
-        DrawShape(Context context) {
-            super(context);
+
+    public void drawShape(View v) {
+
+        switch (v.getId()) {
+            case R.id.but_line:
+                break;
+            case R.id.but_rect:
+                break;
+            case R.id.but_cricle:
+                break;
         }
-        protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-            float cx = getWidth()/2.0f;
-            float cy = getHeight()/2.0f;
-            Paint paint = new Paint();
-            paint.setStrokeWidth(7);
-            paint.setColor(Color.BLUE);
-            paint.setStyle(Paint.Style.STROKE);
-            canvas.drawCircle(cx,cy,200,paint);
-            paint.setColor(Color.MAGENTA);
-            paint.setStyle(Paint.Style.FILL);
-            canvas.drawRect(10,10,210,160,paint);
+    }
+
+        class DrawShape extends View {
+            DrawShape(Context context) {
+                super(context);
+            }
+
+            protected void onDraw(Canvas canvas) {
+                super.onDraw(canvas);
+                float cx = getWidth() / 2.0f;
+                float cy = getHeight() / 2.0f;
+                Paint paint = new Paint();
+                paint.setStrokeWidth(7);
+                paint.setColor(Color.BLUE);
+                paint.setStyle(Paint.Style.STROKE);
+                canvas.drawCircle(cx, cy, 200, paint);
+                paint.setColor(Color.MAGENTA);
+                paint.setStyle(Paint.Style.FILL);
+                canvas.drawRect(10, 10, 210, 160, paint);
+            }
         }
     }
 }
